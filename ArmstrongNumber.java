4260d0ebd5db286a6841e7b9d4812c17f4fee268
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-07-14 15:03:30 +0600

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,26 @@
+
+package AnisulPractice;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum =0,num,temp,r;
+        System.out.print("Enter any Number:");
+        num = input.nextInt();
+        temp = num ;
+        while(temp!= 0){
+            r = temp % 10 ;
+            sum = sum + r*r*r ;
+            temp = temp / 10 ;
+        }
+        if(num==sum)
+        {
+            System.out.println("Armstrong");
+        }else
+        {
+            System.out.println("Not Armstrong:");
+        }
+    }
+}
