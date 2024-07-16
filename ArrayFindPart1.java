39ec01060b01e75fd6a886c7412557fa75200e51
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-07-16 11:18:00 +0600

    Add files via upload

---
+++ ArrayFindPart1.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.arraypractice;
+
+
+public class ArrayFindPart1 {
+    public static void main(String[] args) {
+        int[][] num =new int[4][5];
+        int k=0;
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                num[i][j]=k;
+                k++;
+            }
+        }
+        
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <5; j++) {
+                System.out.print(" "+num[i][j]);
+            }
+            System.out.println();
+        }   
+    } 
+}
+
+
