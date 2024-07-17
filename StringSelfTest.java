052615b508428402ca182eb8a56f9c0729553ebc
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-07-17 19:37:35 +0600

    Add files via upload

---
+++ StringSelfTest.java
@@ -0,0 +1,20 @@
+
+package String_Package;
+
+
+public class StringSelfTest {
+    
+    public static void main(String arg[]){
+    String s1=null;
+    String s2=" Sheikh";
+    //    boolean b=s1.isEmpty();
+//        System.out.println("S1 is empty="+b);
+     
+        if(s1==null || s1.isEmpty()){
+            System.out.println("no value");
+        }else{
+            System.out.println("have some value");
+        }
+    
+    }
+}
