74908e070745c876a77f2c59ec3fc0ccf4b78f42
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-11 12:30:45 +0600

    Add files via upload

---
+++ FactorialTest.java
@@ -0,0 +1,11 @@
+
+package AnisulPractice;
+
+public class FactorialTest {
+       public static void main(String[] args) {
+
+    FactorialDemo ob=new FactorialDemo();
+    int result=ob.fact(5);
+           System.out.println("Factoraial: "+result);
+}
+}
