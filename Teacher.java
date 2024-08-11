4662e99326400c7264153a908911aa2d4c5a3660
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-11 18:02:06 +0600

    Add files via upload

--- Teacher.java
+++ Teacher.java
@@ -1,74 +1,16 @@
-package String_Package;
 
-public class Teacher {
-    private String name;
-    private double age;
-    private String id;
-    private String designation;
-    private String[] phones;
-    private String[] address;
+package Inheritance;
 
-    public String getName() {
-        return this.name;
-    }
 
-    public void setName(String pName) {
-        this.name = pName;
-    }
+public class Teacher extends Person{
+    String qualification;
     
-    public void setNameAgePhones(String nName,double aAge,String[] pPhones)
+void displayInformation()
     {
-        this.name=nName;
-        this.age=aAge;
-        this.phones=pPhones;
-    }
+        System.out.println("Name:"+name);
+                System.out.println("Age:"+age);
+                                System.out.println("Qualification:"+qualification);
 
-    public double getAge() {
-        return this.age;
-    }
 
-    public void setAge(double pAge) {
-        this.age = pAge;
-
-    }
-
-    public String getID() {
-        return this.id;
-    }
-
-    public void setID(String pID) {
-        this.id = pID;
-    }
-
-    public String getDesignation() {
-        return this.designation;
-    }
-
-    public void setDesignation(String pDesignation) {
-        this.designation = pDesignation;
-    }
-
-    public String[] getPhones() {
-        return this.phones;
-    }
-
-    public void setPhones(String[] pPhones) {
-        this.phones = pPhones;
-    }
-
-    public String[] getAddress() {
-        return this.address;
-
-    }
-
-    public void setAddress(String[] aAddress) {
-        this.address = aAddress;
-    }
-
-    @Override
-    public String toString() {
-        return name+" "+age+" "+ phones[0]+","+phones[1]; // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-    
-    
+    }    
 }
