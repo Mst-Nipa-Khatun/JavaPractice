34ebd1fb3bcd7bfcfbacbbb76cd8ceda624e9e97
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-12 15:42:04 +0600

    Add files via upload

---
+++ SuperKeyTest.java
@@ -0,0 +1,19 @@
+
+package SuperKeyword;
+
+
+public class SuperKeyTest {
+    public static void main(String[] args) {
+        //Vichele v1=new Vichele();
+        Car ob1= new Car("red",2.4,5);
+       /* ob1.colour="Red";
+        ob1.weight=2.5;
+        ob1.gear="Volvo";*/
+        
+        ob1.attribute();
+        
+        
+        
+    }
+    
+}
