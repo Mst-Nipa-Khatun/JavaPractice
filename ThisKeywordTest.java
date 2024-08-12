22fc561e58e9e099f46ef4979f2ff20469af80de
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-12 16:41:05 +0600

    Add files via upload

---
+++ ThisKeywordTest.java
@@ -0,0 +1,13 @@
+package ThisKeyword;
+
+public class ThisKeywordTest {
+    public static void main(String[] args) {
+    PersonThisKeyword p1 = new PersonThisKeyword("KIBRIA",28);
+    p1.displayInformation ();
+    
+    PersonThisKeyword p2 = new PersonThisKeyword("NIPA",25,"red");
+    p2.displayInformation ();
+
+    }
+    
+}
