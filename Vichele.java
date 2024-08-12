80c2bbeacd882cee57b12f2b2afa53a1aa7316ba
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-12 15:42:26 +0600

    Add files via upload

---
+++ Vichele.java
@@ -0,0 +1,20 @@
+
+package SuperKeyword;
+
+
+public class Vichele {
+    String colour;
+    double weight;
+   
+    Vichele(String c,double w)
+    {
+        colour=c;
+        weight=w;
+    }
+    void attribute()
+    {
+        System.out.println("Color:"+colour);
+        System.out.println("weight:"+weight);
+    }
+    
+}
