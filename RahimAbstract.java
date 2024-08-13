85f226aef8f89a3090d2e471bf98fa06befc1717
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-13 16:00:59 +0600

    Add files via upload

---
+++ RahimAbstract.java
@@ -0,0 +1,12 @@
+
+package Abstraction;
+
+
+public class RahimAbstract extends MoileUserAbstract {
+
+    @Override
+    void sendMessage() {
+        System.out.println("love you nipa babu babu babu babu babu babu babu babu.............................................................");
+    }
+    
+}
