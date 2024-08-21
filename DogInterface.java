eb1cdce00399bb22f025706712f09bb4b6a19f6a
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-21 13:37:05 +0600

    Add files via upload

---
+++ DogInterface.java
@@ -0,0 +1,13 @@
+
+package Interface;
+
+
+public class DogInterface implements AnimalInterface {
+    
+    @Override
+public void eat()
+    {
+        System.out.println("Dog eat meat ");
+    }
+    
+}
