4778bd187ad6f2870cc9502a093f319fe765996c
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-08-21 13:37:24 +0600

    Add files via upload

---
+++ InterfaceTest.java
@@ -0,0 +1,13 @@
+
+package Interface;
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+    DogInterface d1= new DogInterface();
+    d1.eat();
+    
+    CatInterface c1= new CatInterface();
+    c1.eat();
+}
+    }
+   
